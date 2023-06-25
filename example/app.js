d485ddc7af83afca40ee85879c618aeabfd3fd22
rhys.zhao <rhys.zhao@cloudwise.com>
2023-06-25 19:54:11 +0800

    Update 修复bug

--- example/app.js
+++ example/app.js
@@ -2,13 +2,13 @@
  * Author  rhys.zhao
  * Date  2022-01-28 15:36:55
  * LastEditors  rhys.zhao
- * LastEditTime  2023-06-25 19:07:09
+ * LastEditTime  2023-06-25 19:52:14
  * Description 组件使用例子
  */
 import React from 'react';
 import { render } from 'react-dom';
 
-import { useAutoFit } from '../src';
+import { useAutoFit } from '../lib';
 
 const App = () => {
   useAutoFit(1920, 1080, 'screen');
--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@
  * Author  rhys.zhao
  * Date  2023-02-06 01:45:12
  * LastEditors  rhys.zhao
- * LastEditTime  2023-06-25 19:06:56
+ * LastEditTime  2023-06-25 19:47:34
  * Description 入口文件
  */
 import useAutoFit from './hooks/useAutoFit';
 import useWebSocket from './hooks/useWebSocket';
 
-export { useAutoFit, useWebSocket };
+export default { useAutoFit, useWebSocket };
